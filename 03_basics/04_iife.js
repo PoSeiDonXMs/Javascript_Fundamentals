074870e8b5fa48979819c4e3f1577fa8f35fd828
Madhav Sharma <116294690+PoSeiDonXMs@users.noreply.github.com>
2024-06-01 11:23:32 +0000

    updates

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions
+
+// function chai(){
+//     console.log(`DB Connected`);
+// }
+ 
+// chai();  normally to aise likhte par agar immediately invoke karna ho to uske liye below waala
+
+
+function chai(){
+    console.log(`DB Connected`);
+}
+ 
+chai();
