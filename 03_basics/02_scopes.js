d982c3baa74116f72240775774f3e091e8903a8f
Madhav Sharma <116294690+PoSeiDonXMs@users.noreply.github.com>
2024-06-01 06:28:27 +0000

    updates

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,17 +3,43 @@ let a = 300
 if(true){   // using var is a bad practice since it can also be accessed outside the scope also . this thing can not be done with var and let
     let a = 10
     const b = 20 
-    console.log("INNER", a);
+    // console.log("INNER", a);
     var c = 200
 }
 
-for ( let i = 0; i <array.length;i++){
-    const element = array[i];
-}
+// for ( let i = 0; i <array.length;i++){
+//     const element = array[i];
+// }
 
 console.log(a);
 // console.log(b);
 console.log(c);  // in the output is giving 200 but this shouldnt be possible || only possible because we are using var  
+
+
+
+function one(){
+    const username = "Madhav"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);  ye to error de degi , scope ke bahar hai 
+
+     two()
+
+}
+
+one()
+
+if(true) {
+    const username = "Madhav"
+    if(username === 'Madhav') {
+        const website = "Youtube"
+         console.log(username + website);
+    }
+    // console.log(website);
+}
 
 
 
